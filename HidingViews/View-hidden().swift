0f8b42301de4313647a136f1d276752e4ef14735
George Elsham <george.elsham@elsham.com>
2020-01-15 08:17:52 +0000

    Initial Commit

---
+++ HidingViews/View-hidden().swift
@@ -0,0 +1,43 @@
+//
+//  View-hidden().swift
+//  HidingViews
+//
+//  Created by George Elsham on 15/01/2020.
+//  Copyright © 2020 George Elsham. All rights reserved.
+//
+
+import SwiftUI
+
+
+extension View {
+    
+    /// Whether the view is hidden.
+    /// - Parameter bool: Set to `true` to hide the view. Set to `false` to show the view.
+    func isHidden(_ bool: Bool) -> some View {
+        modifier(HiddenModifier(isHidden: bool))
+    }
+}
+
+
+/// Creates a view modifier that can be applied, like so:
+///
+/// ```
+/// Text("Hello World!")
+///     .isHidden(true)
+/// ```
+///
+/// Variables can be used in place so that the content can be changed dynamically.
+private struct HiddenModifier: ViewModifier {
+
+    fileprivate let isHidden: Bool
+
+    fileprivate func body(content: Content) -> some View {
+        Group {
+            if isHidden {
+                content.hidden()
+            } else {
+                content
+            }
+        }
+    }
+}
