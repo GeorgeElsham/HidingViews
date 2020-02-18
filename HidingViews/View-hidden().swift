94abc9b9bbf895f210109dd6efbb9b2fc61dbd6f
Craz1k0ek <bram_kolkman@hotmail.com>
2020-02-18 15:59:21 +0100

    Added flag to completely remove view
    + Updated the HiddenModifier with a new removal flag
    + Updated the HiddenModifier body to handle the removal flag
    + Updated the View extension to support the new removal flag
    + Updated code comments to make them more supportive when using them in files

--- HidingViews/View-hidden().swift
+++ HidingViews/View-hidden().swift
@@ -10,34 +10,56 @@ import SwiftUI
 
 
 extension View {
-    
-    /// Whether the view is hidden.
-    /// - Parameter bool: Set to `true` to hide the view. Set to `false` to show the view.
-    func isHidden(_ bool: Bool) -> some View {
-        modifier(HiddenModifier(isHidden: bool))
+        
+    /// Hide or show the view based on a boolean value.
+    ///
+    /// Example for visibility:
+    /// ```
+    /// Text("Label")
+    ///     .isHidden(true)
+    /// ```
+    ///
+    /// Example for complete removal:
+    /// ```
+    /// Text("Label")
+    ///     .isHidden(true, remove: true)
+    /// ```
+    ///
+    /// - Parameters:
+    ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
+    ///   - remove: Boolean value indicating whether or not to remove the view.
+    func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        modifier(HiddenModifier(isHidden: hidden, remove: remove))
     }
+    
 }
 
 
-/// Creates a view modifier that can be applied, like so:
-///
-/// ```
-/// Text("Hello World!")
-///     .isHidden(true)
-/// ```
+/// Creates a view modifier to show and hide a view.
 ///
 /// Variables can be used in place so that the content can be changed dynamically.
-private struct HiddenModifier: ViewModifier {
-
+fileprivate struct HiddenModifier: ViewModifier {
+    
     fileprivate let isHidden: Bool
-
+    fileprivate let remove: Bool
+    
+    init(isHidden: Bool, remove: Bool = false) {
+        self.isHidden   = isHidden
+        self.remove     = remove
+    }
+    
     fileprivate func body(content: Content) -> some View {
         Group {
             if isHidden {
-                content.hidden()
+                if remove {
+                    EmptyView()
+                } else {
+                    content.hidden()
+                }
             } else {
                 content
             }
         }
     }
+    
 }
