21516d5a403cc348b2e19b46cfa81f1f1c9b5eb5
George-J-E <george.elsham@elsham.com>
2020-08-10 01:26:59 +0100

    Simplified into one function.
    Using `@ViewBuilder` because it is higher performance than wrapping in a `Group`.

--- Sources/HidingViews/View-hidden.swift
+++ Sources/HidingViews/View-hidden.swift
@@ -28,37 +28,13 @@ public extension View {
     /// - Parameters:
     ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
     ///   - remove: Boolean value indicating whether or not to remove the view.
-    func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
-        modifier(HiddenModifier(isHidden: hidden, remove: remove))
-    }
-}
-
-
-/// Creates a view modifier to show and hide a view.
-///
-/// Variables can be used in place so that the content can be changed dynamically.
-fileprivate struct HiddenModifier: ViewModifier {
-    
-    private let isHidden: Bool
-    private let remove: Bool
-    
-    init(isHidden: Bool, remove: Bool = false) {
-        self.isHidden = isHidden
-        self.remove = remove
-    }
-    
-    func body(content: Content) -> some View {
-        Group {
-            if isHidden {
-                if remove {
-                    EmptyView()
-                } else {
-                    content.hidden()
-                }
-            } else {
-                content
+    @ViewBuilder func isHidden(_ hidden: Bool, remove: Bool = false) -> some View {
+        if hidden {
+            if !remove {
+                self.hidden()
             }
+        } else {
+            self
         }
     }
-    
 }
