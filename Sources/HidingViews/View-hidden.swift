c21f613c2640d831b58ca8637ceb736acd5df7d3
George-J-E <george.elsham@elsham.com>
2021-03-03 10:37:50 +0000

    Clearer documentation.

--- Sources/HidingViews/View-hidden.swift
+++ Sources/HidingViews/View-hidden.swift
@@ -14,16 +14,14 @@ public extension View {
     /// Hide or show the view based on a boolean value.
     ///
     /// Example for visibility:
-    /// ```
-    /// Text("Label")
-    ///     .isHidden(true)
-    /// ```
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true)
     ///
     /// Example for complete removal:
-    /// ```
-    /// Text("Label")
-    ///     .isHidden(true, remove: true)
-    /// ```
+    ///
+    ///     Text("Label")
+    ///         .isHidden(true, remove: true)
     ///
     /// - Parameters:
     ///   - hidden: Set to `false` to show the view. Set to `true` to hide the view.
